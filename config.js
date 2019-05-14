701b0101deb2eb41c6397aca844dad7653e03ee5
HugoRoca <hugo.rock20@hotmail.com>
2019-05-14 09:56:58 -0500

    add new file config

---
+++ config.js
@@ -0,0 +1,7 @@
+const config = {
+    mongo: {
+        url: "mongodb+srv://hugoroca:cutu*2021@cluster0-xu1hg.mongodb.net/test"
+    }
+}
+
+module.exports = config;
--- index.js
+++ index.js
@@ -1,2 +1,5 @@
-import { mongoClient } from "mongodb";
+const mongoClient = require("mongodb").MongoClient;
+const config = require("./config");
+
+console.log(config.mongo.url);
 
