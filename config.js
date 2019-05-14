b8e66b0092f88dea047eac737d9ea66c47d0a85a
HugoRoca <hugo.rock20@hotmail.com>
2019-05-14 14:43:19 -0500

    edit files

--- config.js
+++ config.js
@@ -1,6 +1,8 @@
 const config = {
     mongo: {
-        url: "mongodb+srv://hugoroca:cutu*2021@cluster0-xu1hg.mongodb.net/test"
+        url: "mongodb+srv://hugoroca:cutu*2021@cluster0-xu1hg.mongodb.net/test",
+        database: "StreamLog",
+        collection: "Logs"
     }
 }
 
--- mongoUtils.js
+++ mongoUtils.js
@@ -1,2 +1,18 @@
 const mongoClient = require("mongodb").MongoClient;
 const config = require("./config");
+
+module.exports = class MongoUtils {
+    getConnectionString(){
+        return config.mongo.url;
+    }
+
+    getDataBaseString(){
+        return config.mongo.database;
+    }
+
+    getCollectionString(){
+        return config.mongo.collection;
+    }
+
+    
+}
