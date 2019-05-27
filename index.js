6c98ef0249bfd88ea992c6630ef904950718d573
HugoRoca <hugo.rock20@hotmail.com>
2019-05-27 17:01:52 -0500

    inserción en mongo

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-const mongoClient = require("mongodb").MongoClient;
-const config = require("./config");
+const mongoUtils = require("./mongoUtils");
+const mongo = new mongoUtils();
 
-console.log(config.mongo.url);
+(async() => {
+    let insert = await mongo.insert("Ejecutando mongo desde nodeJS 1");
+    console.log(insert);
+})();
 
--- mongoUtils.js
+++ mongoUtils.js
@@ -2,17 +2,46 @@ const mongoClient = require("mongodb").MongoClient;
 const config = require("./config");
 
 module.exports = class MongoUtils {
-    getConnectionString(){
+    getConnectionString() {
         return config.mongo.url;
     }
 
-    getDataBaseString(){
+    getDataBaseString() {
         return config.mongo.database;
     }
 
-    getCollectionString(){
+    getCollectionString() {
         return config.mongo.collection;
     }
 
-    
+    buildCollection(message) {
+        return {
+            message,
+            createdAt: new Date()
+        }
+    }
+
+    async insertData(data) {
+        return new Promise((resolve, reject) => {
+            mongoClient.connect(this.getConnectionString(), { useNewUrlParser: true }).then(client => {
+
+                const db = client.db(this.getDataBaseString());
+                const collection = db.collection(this.getCollectionString());
+
+                collection.insertOne(data, (insertError, insertResponse) => {
+                    if (insertError) reject(insertError);
+                    resolve(insertResponse);
+                });
+
+            }).catch(err => {
+                console.error("Error al conectarse con la base de datos: ", err);
+            });
+        });
+    }
+
+    async insert(message){
+        let data = this.buildCollection(message);
+        return await this.insertData(data);
+    }
+
 }
